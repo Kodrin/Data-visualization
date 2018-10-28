3e4360ae02a31178322dd389edefc17d2a94fcb3
Kodrin <kodrintxps@gmail.com>
2018-10-28 17:26:30 -0400

    lib commit
    - added dots as data points
    - grid and sum other bullshit

--- js/base.js
+++ js/base.js
@@ -1,30 +1,68 @@
 window.onload = function(){
+  //grid var
+  var gridX = 100;
+  var gridY = 100;
+  var gridZ = 100;
+  var gridSpacing = 5;
 
-  let changeParagraph = function (event){
-    if(this.id == "c"){
-      console.log("c was click again!");
-      pText.style = "display:inline-block";
-      pText.textContent = "This is the c block";
-    } else if (this.id == "d") {
-      console.log("d was click again!");
-      pText.style = "display:inline-block";
-      pText.textContent = "This is the d block";
-    } else if (this.id == "e") {
-      console.log("e was click again!");
-      pText.style = "display:inline-block";
-      pText.textContent = "This is the e block";
-    } else if (this.id == "displayText") {
-      pText.style = "display:none";
-    }
+  var size = 50;
+  var divisions = 50;
+
+  //object storage
+  // var gridDot = [];
+
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  var controls = new THREE.OrbitControls( camera );
+
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  var cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
+  var gridDotGeo = new THREE.SphereGeometry( 0.1, 32, 32 );
+  var gridDotMat = new THREE.MeshBasicMaterial( { color: 0xffffff } )
+
+  var gridHelper = new THREE.GridHelper( size, divisions );
+  scene.add( gridHelper );
+
+  var plane = new THREE.Plane( new THREE.Vector3( 1, 1, 0.2 ), 3 );
+  var helper = new THREE.PlaneHelper( plane, 1, 0xffff00 );
+  scene.add( helper );
+
+  //background
+  for (var i = 0; i < gridX; i++) {
+        var gridDot = new THREE.Mesh (gridDotGeo, gridDotMat);
+        gridDot.position.x = Math.random() * 800 - 400;
+        gridDot.position.y = Math.random() * 800 - 400;
+        gridDot.position.z = Math.random() * 800 - 400;
+        scene.add( gridDot );
   }
 
-  let cBox = document.getElementById('c');
-  cBox.addEventListener('click', changeParagraph);
-  let dBox = document.getElementById('d');
-  dBox.addEventListener('click', changeParagraph);
-  let eBox = document.getElementById('e');
-  eBox.addEventListener('click', changeParagraph);
-  let pText = document.getElementById('displayText');
-  pText.addEventListener('click', changeParagraph);
+  //data point
+  for (var i = 0; i < gridX; i++) {
+        var gridDot = new THREE.Mesh (gridDotGeo, gridDotMat);
+        gridDot.position.x = Math.random() * size -25;
+        gridDot.position.y = Math.random() * size -25;
+        gridDot.position.z = Math.random() * size -25;
+        scene.add( gridDot );
+  }
+
+  camera.position.set( 0, 20, 100 );
+  controls.update();
+
+  var animate = function () {
+    requestAnimationFrame( animate );
+    controls.update();
+
+    renderer.render( scene, camera );
+  };
+
+  animate();
+  // renderer.render( scene, camera );
 
 }
