5f90f0ceb72ce095e2a72bd361ce40a89a658087
Kodrin <kodrintxps@gmail.com>
2018-10-28 18:37:41 -0400

    added postprocessing and shaders

--- js/base.js
+++ js/base.js
@@ -5,31 +5,37 @@ window.onload = function(){
   var gridZ = 100;
   var gridSpacing = 5;
 
-  var size = 50;
-  var divisions = 50;
+  var gridHelperSize = 50;
+  var gridHelperDivisions = 50;
 
   //object storage
   // var gridDot = [];
 
+  //three.js essemtials
   var scene = new THREE.Scene();
   var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
   var controls = new THREE.OrbitControls( camera );
 
   var renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   document.body.appendChild( renderer.domElement );
 
+  //sample cube from example
   var geometry = new THREE.BoxGeometry( 1, 1, 1 );
   var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
   var cube = new THREE.Mesh( geometry, material );
   scene.add( cube );
 
+  //data points look
   var gridDotGeo = new THREE.SphereGeometry( 0.1, 32, 32 );
   var gridDotMat = new THREE.MeshBasicMaterial( { color: 0xffffff } )
 
-  var gridHelper = new THREE.GridHelper( size, divisions );
+  //defines the grid
+  var gridHelper = new THREE.GridHelper( gridHelperSize, gridHelperSize );
   scene.add( gridHelper );
 
+  //plane helper
   var plane = new THREE.Plane( new THREE.Vector3( 1, 1, 0.2 ), 3 );
   var helper = new THREE.PlaneHelper( plane, 1, 0xffff00 );
   scene.add( helper );
@@ -46,10 +52,29 @@ window.onload = function(){
   //data point
   for (var i = 0; i < gridX; i++) {
         var gridDot = new THREE.Mesh (gridDotGeo, gridDotMat);
-        gridDot.position.x = Math.random() * size -25;
-        gridDot.position.y = Math.random() * size -25;
-        gridDot.position.z = Math.random() * size -25;
+        gridDot.position.x = Math.random() * gridHelperSize -gridHelperSize/2;
+        gridDot.position.y = Math.random() * gridHelperSize -gridHelperSize/2;
+        gridDot.position.z = Math.random() * gridHelperSize -gridHelperSize/2;
         scene.add( gridDot );
+  }
+
+  //post processing
+  composer = new THREE.EffectComposer( renderer );
+  composer.addPass( new THREE.RenderPass( scene, camera ) );
+  var effect = new THREE.ShaderPass( THREE.DotScreenShader );
+  effect.uniforms[ 'scale' ].value = 4;
+  composer.addPass( effect );
+  var effect = new THREE.ShaderPass( THREE.RGBShiftShader );
+  effect.uniforms[ 'amount' ].value = 0.0015;
+  effect.renderToScreen = true;
+  composer.addPass( effect );
+  //
+  window.addEventListener( 'resize', onWindowResize, false );
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    composer.setSize( window.innerWidth, window.innerHeight );
   }
 
   camera.position.set( 0, 20, 100 );
@@ -60,6 +85,7 @@ window.onload = function(){
     controls.update();
 
     renderer.render( scene, camera );
+    composer.render();
   };
 
   animate();
