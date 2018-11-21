d203695144d28bfa0e44ed870377f3f53fd7e2c8
Valerie <valerietbourdon@gmail.com>
2018-11-20 19:06:32 -0500

    styling updates

--- datavis/js/search.js
+++ datavis/js/search.js
@@ -1,7 +1,7 @@
 function openNav() {
-  document.getElementById("myNav").style.display = "block";
+  document.getElementById("searchBar").style.display = "block";
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.display = "none";
+  document.getElementById("searchBar").style.display = "none";
 }
